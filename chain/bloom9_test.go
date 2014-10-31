0552ed7a77265a46e9abbd3d6f08bd4a945d94e2
Felix Lange <fjl@twurst.com>
2014-10-31 18:56:25 +0100

    chain, tests/helper, vm: make tests compile
    They were broken by e903d216c56463a8b, when vm.Log became ethstate.Log.

--- chain/bloom9_test.go
+++ chain/bloom9_test.go
@@ -3,12 +3,14 @@ package chain
 import (
 	"testing"
 
-	"gitlab.com/q-dev/q-client/vm"
+	"gitlab.com/q-dev/q-client/state"
 )
 
 func TestBloom9(t *testing.T) {
 	testCase := []byte("testtest")
-	bin := LogsBloom([]vm.Log{vm.Log{testCase, [][]byte{[]byte("hellohello")}, nil}}).Bytes()
+	bin := LogsBloom([]state.Log{
+		{testCase, [][]byte{[]byte("hellohello")}, nil},
+	}).Bytes()
 	res := BloomLookup(bin, testCase)
 
 	if !res {
