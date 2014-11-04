fa9e08b2eec1cfee3963db4c60b68b4fb25cd097
obscuren <geffobscura@gmail.com>
2014-11-04 01:29:49 +0100

    Sha addresses

--- chain/bloom9_test.go
+++ chain/bloom9_test.go
@@ -1,9 +1,12 @@
 package chain
 
 import (
+	"fmt"
 	"testing"
 
+	"gitlab.com/q-dev/q-client/crypto"
 	"gitlab.com/q-dev/q-client/state"
+	"github.com/ethgo.old/ethutil"
 )
 
 func TestBloom9(t *testing.T) {
@@ -16,4 +19,13 @@ func TestBloom9(t *testing.T) {
 	if !res {
 		t.Errorf("Bloom lookup failed")
 	}
+}
+
+func TestAddress(t *testing.T) {
+	block := &Block{}
+	block.Coinbase = ethutil.Hex2Bytes("22341ae42d6dd7384bc8584e50419ea3ac75b83f")
+	fmt.Printf("%x\n", crypto.Sha3(block.Coinbase))
+
+	bin := CreateBloom(block)
+	fmt.Printf("bin = %x\n", ethutil.LeftPadBytes(bin, 64))
 }
