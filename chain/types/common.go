c6006b68f1614547fdc65b202708a350c414ff69
obscuren <geffobscura@gmail.com>
2014-12-03 14:05:19 +0100

    updated to types

--- chain/types/common.go
+++ chain/types/common.go
@@ -2,9 +2,10 @@ package types
 
 import (
 	"math/big"
+
 	"gitlab.com/q-dev/q-client/state"
 )
 
 type BlockProcessor interface {
-	ProcessWithParent(*Block, *Block) (*big.Int, state.Messages, error)
+	Process(*Block) (*big.Int, state.Messages, error)
 }
