98e057ae33d81903031a6ced653921bfb2a26b78
obscuren <geffobscura@gmail.com>
2014-12-18 13:22:59 +0100

    Merge fixes

--- core/types/common.go
+++ core/types/common.go
@@ -4,13 +4,8 @@ import (
 	"math/big"
 
 	"gitlab.com/q-dev/q-client/state"
-	"gitlab.com/q-dev/q-client/wire"
 )
 
 type BlockProcessor interface {
 	Process(*Block) (*big.Int, state.Messages, error)
-}
-
-type Broadcaster interface {
-	Broadcast(wire.MsgType, []interface{})
 }
