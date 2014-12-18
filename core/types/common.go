71b0125b1644c1d9749448c0f953b7ca3e4e9f76
obscuren <geffobscura@gmail.com>
2014-12-18 13:12:54 +0100

    Locks, refactor, tests
    * Added additional chain tests
    * Added proper mutex' on chain
    * Removed ethereum dependencies

--- core/types/common.go
+++ core/types/common.go
@@ -4,8 +4,13 @@ import (
 	"math/big"
 
 	"gitlab.com/q-dev/q-client/state"
+	"gitlab.com/q-dev/q-client/wire"
 )
 
 type BlockProcessor interface {
 	Process(*Block) (*big.Int, state.Messages, error)
+}
+
+type Broadcaster interface {
+	Broadcast(wire.MsgType, []interface{})
 }
