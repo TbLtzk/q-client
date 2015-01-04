319a1ffb2b1a3fa7f415ae8afcfbfc890b05dab6
obscuren <geffobscura@gmail.com>
2015-01-04 14:20:16 +0100

    Cleaned up some of that util

--- core/block_manager.go
+++ core/block_manager.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"math/big"
 	"sync"
-	"time"
 
 	"gitlab.com/q-dev/q-client/core/types"
 	"gitlab.com/q-dev/q-client/crypto"
@@ -21,18 +20,6 @@ import (
 )
 
 var statelogger = logger.NewLogger("BLOCK")
-
-type Peer interface {
-	Inbound() bool
-	LastSend() time.Time
-	LastPong() int64
-	Host() []byte
-	Port() uint16
-	Version() string
-	PingTime() string
-	Connected() *int32
-	Caps() *ethutil.Value
-}
 
 type EthManager interface {
 	BlockManager() *BlockManager
