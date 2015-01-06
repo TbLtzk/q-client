1f2f8b174c61f45344a7987797edaf1353fa137e
obscuren <geffobscura@gmail.com>
2015-01-06 11:44:22 +0100

    Updated WS API. Fixes #219. Closes #220

--- websocket/message.go
+++ websocket/message.go
@@ -5,7 +5,7 @@ import "gitlab.com/q-dev/q-client/ethutil"
 type Message struct {
 	Call string        `json:"call"`
 	Args []interface{} `json:"args"`
-	Seed int           `json:"seed"`
+	Id   int           `json:"_id"`
 	Data interface{}   `json:"data"`
 }
 
