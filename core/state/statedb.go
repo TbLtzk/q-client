29db455d1bc2e4678ea6d940b0c627cd45333dec
obscuren <geffobscura@gmail.com>
2015-04-03 17:09:01 +0200

    glog wip

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -7,6 +7,7 @@ import (
 	"gitlab.com/q-dev/q-client/common"
 	"gitlab.com/q-dev/q-client/logger"
 	"gitlab.com/q-dev/q-client/trie"
+	"github.com/golang/glog"
 )
 
 var statelogger = logger.NewLogger("STATE")
@@ -210,7 +211,9 @@ func (self *StateDB) GetOrNewStateObject(addr common.Address) *StateObject {
 
 // NewStateObject create a state object whether it exist in the trie or not
 func (self *StateDB) newStateObject(addr common.Address) *StateObject {
-	statelogger.Debugf("(+) %x\n", addr)
+	if glog.V(logger.Debug) {
+		glog.Infof("(+) %x\n", addr)
+	}
 
 	stateObject := NewStateObject(addr, self.db)
 	self.stateObjects[addr.Str()] = stateObject
