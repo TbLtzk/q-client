7af0d954510e65404c3bfdd4702438f07a85a218
Péter Szilágyi <peterke@gmail.com>
2015-11-17 18:33:25 +0200

    cmd, common, core, eth, node, rpc, tests, whisper, xeth: use protocol stacks

--- xeth/state.go
+++ xeth/state.go
@@ -19,6 +19,7 @@ package xeth
 import (
 	"gitlab.com/q-dev/q-client/common"
 	"gitlab.com/q-dev/q-client/core/state"
+	"gitlab.com/q-dev/q-client/eth"
 )
 
 type State struct {
@@ -45,8 +46,7 @@ func (self *State) SafeGet(addr string) *Object {
 func (self *State) safeGet(addr string) *state.StateObject {
 	object := self.state.GetStateObject(common.HexToAddress(addr))
 	if object == nil {
-		object = state.NewStateObject(common.HexToAddress(addr), self.xeth.backend.ChainDb())
+		object = state.NewStateObject(common.HexToAddress(addr), self.xeth.backend.Service("eth").(*eth.Ethereum).ChainDb())
 	}
-
 	return object
 }
