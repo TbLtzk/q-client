bradberger/go-ethereum

eth: separate common and full node-specific API and backend service

eth/gpu_mining.go
@@ -33,7 +33,7 @@ import (
 	"github.com/ethereum/go-ethereum/miner"
 )
 
-func (s *Ethereum) StartMining(threads int, gpus string) error {
+func (s *FullEthereum) StartMining(threads int, gpus string) error {
 	eb, err := s.Etherbase()
 	if err != nil {
 		err = fmt.Errorf("Cannot start mining without etherbase address: %v", err)

TbLtzk/q-client

eth/gpu_mining.go
@@ -33,7 +33,7 @@ import (
 	"gitlab.com/q-dev/q-client/miner"
 )
 
-func (s *Ethereum) StartMining(threads int, gpus string) error {
+func (s *FullNodeService) StartMining(threads int, gpus string) error {
 	eb, err := s.Etherbase()
 	if err != nil {
 		err = fmt.Errorf("Cannot start mining without etherbase address: %v", err)