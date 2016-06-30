7b186a7b335834c3662ee2af29e2944700a4fc1e
Péter Szilágyi <peterke@gmail.com>
2016-06-30 13:03:26 +0300

    cmd, common, console, eth, release: drop redundant "full"s

--- eth/gpu_mining.go
+++ eth/gpu_mining.go
@@ -33,7 +33,7 @@ import (
 	"gitlab.com/q-dev/q-client/miner"
 )
 
-func (s *FullNodeService) StartMining(threads int, gpus string) error {
+func (s *Ethereum) StartMining(threads int, gpus string) error {
 	eb, err := s.Etherbase()
 	if err != nil {
 		err = fmt.Errorf("Cannot start mining without etherbase address: %v", err)
