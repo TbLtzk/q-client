a4d982eb1d64fd5fa95bb0f339346f3b459374ee
Felix Lange <fjl@twurst.com>
2016-08-15 20:14:05 +0200

    core, miner: move Backend to miner
    This ensures that package core doesn't depend on package accounts and
    resolves an age-old TODO.

--- core/types.go
+++ core/types.go
@@ -19,12 +19,9 @@ package core
 import (
 	"math/big"
 
-	"gitlab.com/q-dev/q-client/accounts"
 	"gitlab.com/q-dev/q-client/core/state"
 	"gitlab.com/q-dev/q-client/core/types"
 	"gitlab.com/q-dev/q-client/core/vm"
-	"gitlab.com/q-dev/q-client/ethdb"
-	"gitlab.com/q-dev/q-client/event"
 )
 
 // Validator is an interface which defines the standard for block validation.
@@ -62,16 +59,4 @@ type HeaderValidator interface {
 // failed.
 type Processor interface {
 	Process(block *types.Block, statedb *state.StateDB, cfg vm.Config) (types.Receipts, vm.Logs, *big.Int, error)
-}
-
-// Backend is an interface defining the basic functionality for an operable node
-// with all the functionality to be a functional, valid Ethereum operator.
-//
-// TODO Remove this
-type Backend interface {
-	AccountManager() *accounts.Manager
-	BlockChain() *BlockChain
-	TxPool() *TxPool
-	ChainDb() ethdb.Database
-	EventMux() *event.TypeMux
 }
