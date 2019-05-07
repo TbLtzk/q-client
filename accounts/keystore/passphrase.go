19890dc33e40be56fbd3dc14231a29796e5ae80a
Kurkó Mihály <kurkomisi@users.noreply.github.com>
2019-05-07 15:49:51 +0300

    accounts, cmd, internal, signer: add note about backing up the keystore (#19432)
    * accounts: add note about backing up the keystore
    
    * cmd, accounts: move the printout to accountCreate
    
    * internal, signer: add info when new account is created via rpc
    
    * cmd, internal, signer: split logs
    
    * cmd/geth: make account new output a bit more verbose

--- accounts/keystore/passphrase.go
+++ accounts/keystore/passphrase.go
@@ -38,6 +38,7 @@ import (
 	"os"
 	"path/filepath"
 
+	"gitlab.com/q-dev/q-client/accounts"
 	"gitlab.com/q-dev/q-client/common"
 	"gitlab.com/q-dev/q-client/common/math"
 	"gitlab.com/q-dev/q-client/crypto"
@@ -97,9 +98,9 @@ func (ks keyStorePassphrase) GetKey(addr common.Address, filename, auth string) 
 }
 
 // StoreKey generates a key, encrypts with 'auth' and stores in the given directory
-func StoreKey(dir, auth string, scryptN, scryptP int) (common.Address, error) {
+func StoreKey(dir, auth string, scryptN, scryptP int) (accounts.Account, error) {
 	_, a, err := storeNewKey(&keyStorePassphrase{dir, scryptN, scryptP, false}, rand.Reader, auth)
-	return a.Address, err
+	return a, err
 }
 
 func (ks keyStorePassphrase) StoreKey(filename string, key *Key, auth string) error {
