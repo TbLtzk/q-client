1f8c490c9f86f3b2d304d0ebd6401aa9a9936655
Guillaume Ballet <gballet@gmail.com>
2019-11-23 12:51:37 +0100

    tests, signer: remove staticcheck warnings (#20364)

--- signer/core/stdioui.go
+++ signer/core/stdioui.go
@@ -18,7 +18,6 @@ package core
 
 import (
 	"context"
-	"sync"
 
 	"gitlab.com/q-dev/q-client/internal/ethapi"
 	"gitlab.com/q-dev/q-client/log"
@@ -27,7 +26,6 @@ import (
 
 type StdIOUI struct {
 	client rpc.Client
-	mu     sync.Mutex
 }
 
 func NewStdIOUI() *StdIOUI {
