741bec90719a04a6528769afe54c233bd6d78854
Paweł Bylica <chfast@gmail.com>
2020-06-08 14:24:40 +0200

    core/vm: use uint256 in EVM implementation (#20787)
    * core/vm: use fixed uint256 library instead of big
    
    * core/vm: remove intpools
    
    * core/vm: upgrade uint256, fixes uint256.NewFromBig
    
    * core/vm: use uint256.Int by value in Stack
    
    * core/vm: upgrade uint256 to v1.0.0
    
    * core/vm: don't preallocate space for 1024 stack items (only 16)
    
    Co-authored-by: Martin Holst Swende <martin@swende.se>

--- core/vm/logger_test.go
+++ core/vm/logger_test.go
@@ -23,6 +23,8 @@ import (
 	"gitlab.com/q-dev/q-client/common"
 	"gitlab.com/q-dev/q-client/core/state"
 	"gitlab.com/q-dev/q-client/params"
+
+	"github.com/holiman/uint256"
 )
 
 type dummyContractRef struct {
@@ -57,8 +59,8 @@ func TestStoreCapture(t *testing.T) {
 		rstack   = newReturnStack()
 		contract = NewContract(&dummyContractRef{}, &dummyContractRef{}, new(big.Int), 0)
 	)
-	stack.push(big.NewInt(1))
-	stack.push(big.NewInt(0))
+	stack.push(uint256.NewInt().SetUint64(1))
+	stack.push(uint256.NewInt())
 	var index common.Hash
 	logger.CaptureState(env, 0, SSTORE, 0, 0, mem, stack, rstack, contract, 0, nil)
 	if len(logger.changedValues[contract.Address()]) == 0 {
