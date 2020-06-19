ee56142dd875eb5ec5ed06b2d2af5b7bf006e014
Sergey Gimonov <xr9kayu@gmail.com>
2020-06-19 15:30:19 +0300

    Feature/ add wrapper for validators smart contract

--- contracts/validators/validators.go
+++ contracts/validators/validators.go
@@ -1,1 +1,31 @@
 package validators
+
+import (
+	"gitlab.com/q-dev/go-ethereum/accounts/abi/bind"
+	"gitlab.com/q-dev/go-ethereum/common"
+	"gitlab.com/q-dev/go-ethereum/contracts/validators/contract"
+	"math/big"
+)
+
+// Validators is a Go wrapper around an on-chain validator contract.
+type Validators struct {
+	address  common.Address
+	contract *contract.Validators
+}
+
+// NewValidators binds contract and returns a registrar instance.
+func NewValidators(contractAddr common.Address, backend bind.ContractBackend) (*Validators, error) {
+	c, err := contract.NewValidators(contractAddr, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Validators{address: contractAddr, contract: c}, nil
+}
+
+func (validators *Validators) GetValidatorsList() ([]common.Address, error) {
+	return validators.contract.GetValidatorsList(&bind.CallOpts{})
+}
+
+func (validators *Validators) DelegateStakeToValidator(address common.Address, amount *big.Int) error {
+	//TODO:implement
+}
