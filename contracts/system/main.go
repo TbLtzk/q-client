4533b0f4401c4245e97cd32937271cc00daf5fbe
Skriabin <artem.skriabin@distributedlab.com>
2020-06-19 11:41:02 +0300

    QDEV-354 WIP: Add func to deploy system contracts (wait for contracts)

---
+++ contracts/system/main.go
@@ -0,0 +1,54 @@
+package system
+
+import (
+	"math/big"
+
+	"gitlab.com/q-dev/go-ethereum/contracts/checkpointvalidators/contract"
+
+	"gitlab.com/q-dev/go-ethereum/accounts/abi/bind"
+	"gitlab.com/q-dev/go-ethereum/common"
+	"gitlab.com/q-dev/go-ethereum/core/types"
+	"gitlab.com/q-dev/go-ethereum/crypto"
+
+	"gitlab.com/q-dev/go-ethereum/ethclient"
+
+	"gitlab.com/q-dev/go-ethereum/cmd/utils"
+
+	"gitlab.com/q-dev/go-ethereum/log"
+)
+
+// address 532c69263800e1f1cdb72acae555a85864146986
+const privateKeyHex = "e4160a6c21bc6c70a93f07c42c540fcaaf4ab973b75e12a6dbe2b415a933cd6a"
+
+type deployFunc func(*bind.TransactOpts, bind.ContractBackend) (common.Address, *types.Transaction, *contract.Validators, error)
+
+var contracts = []deployFunc{contract.DeployValidators}
+
+func DeploySystemContracts(client *ethclient.Client) {
+
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		utils.Fatalf("Failed to deploy checkpoint oracle %v", err)
+	}
+
+	// setup clef signer, create an abigen transactor and an RPC client
+	transactor := &bind.TransactOpts{
+		From: crypto.PubkeyToAddress(privateKey.PublicKey),
+		Signer: func(signer types.Signer, address common.Address, tx *types.Transaction,
+		) (*types.Transaction, error) {
+			return types.SignTx(tx, types.HomesteadSigner{}, privateKey)
+		},
+		Nonce: big.NewInt(0),
+	}
+
+	for _, sysContract := range contracts {
+		address, tx, _, err := sysContract(transactor, client)
+		if err != nil {
+			log.Warn("Failed to system contract oracle", "error", err)
+		}
+		log.Info("Deployed system contract", "address", address, "tx", tx.Hash().Hex())
+
+		transactor.Nonce.Add(transactor.Nonce, big.NewInt(1))
+	}
+
+}
