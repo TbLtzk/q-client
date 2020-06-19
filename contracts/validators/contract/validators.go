319e66b7fd7f711016940232481d8ebb31e95f39
Sergey Gimonov <xr9kayu@gmail.com>
2020-06-19 15:28:49 +0300

    Feature/ update smart contract

--- contracts/validators/contract/validators.go
+++ contracts/validators/contract/validators.go
@@ -28,16 +28,16 @@ var (
 )
 
 // ValidatorsABI is the input ABI used to generate the binding from.
-const ValidatorsABI = "[{\"constant\":true,\"inputs\":[{\"name\":\"validatorPubkey\",\"type\":\"string\"}],\"name\":\"addValidator\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"getValidators\",\"outputs\":[{\"name\":\"\",\"type\":\"string[]\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]"
+const ValidatorsABI = "[{\"constant\":true,\"inputs\":[],\"name\":\"getValidatorsList\",\"outputs\":[{\"name\":\"\",\"type\":\"address[]\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"validatorAddress\",\"type\":\"address\"}],\"name\":\"addValidator\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
 
 // ValidatorsFuncSigs maps the 4-byte function signature to its string representation.
 var ValidatorsFuncSigs = map[string]string{
-	"b5da04f5": "addValidator(string)",
-	"b7ab4db5": "getValidators()",
+	"4d238c8e": "addValidator(address)",
+	"3d620628": "getValidatorsList()",
 }
 
 // ValidatorsBin is the compiled bytecode used for deploying new contracts.
-var ValidatorsBin = "0x610140604052608060a08181527f306138346535363263396232326534333236396237646361323135636632656460c0527f386332306262663335646136376261653864356565383162333664386262623660e0527f3965336563373034623962366637353031303539666538363138343361383336610100527f62326662616236343166333636313663646437373336356231613532326435626101205281526100ae9060009060016100c1565b503480156100bb57600080fd5b5061021f565b82805482825590600052602060002090810192821561010e579160200282015b8281111561010e57825180516100fe91849160209091019061011e565b50916020019190600101906100e1565b5061011a929150610198565b5090565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061015f57805160ff191683800117855561018c565b8280016001018555821561018c579182015b8281111561018c578251825591602001919060010190610171565b5061011a9291506101be565b6101bb91905b8082111561011a5760006101b282826101d8565b5060010161019e565b90565b6101bb91905b8082111561011a57600081556001016101c4565b50805460018160011615610100020316600290046000825580601f106101fe575061021c565b601f01602090049060005260206000209081019061021c91906101be565b50565b6104728061022e6000396000f30060806040526004361061004b5763ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663b5da04f58114610050578063b7ab4db514610072575b600080fd5b34801561005c57600080fd5b5061007061006b3660046102a8565b61009d565b005b34801561007e57600080fd5b506100876100e3565b6040516100949190610381565b60405180910390f35b600080546001810180835591805282516100de917f290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563019060208501906101bc565b505050565b60606000805480602002602001604051908101604052809291908181526020016000905b828210156101b25760008481526020908190208301805460408051601f600260001961010060018716150201909416939093049283018590048502810185019091528181529283018282801561019e5780601f106101735761010080835404028352916020019161019e565b820191906000526020600020905b81548152906001019060200180831161018157829003601f168201915b505050505081526020019060010190610107565b5050505090505b90565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106101fd57805160ff191683800117855561022a565b8280016001018555821561022a579182015b8281111561022a57825182559160200191906001019061020f565b5061023692915061023a565b5090565b6101b991905b808211156102365760008155600101610240565b6000601f8201831361026557600080fd5b8135610278610273826103c0565b610399565b9150808252602083016020830185838301111561029457600080fd5b61029f8382846103f2565b50505092915050565b6000602082840312156102ba57600080fd5b813567ffffffffffffffff8111156102d157600080fd5b6102dd84828501610254565b949350505050565b60006102f0826103ee565b80845260208401935083602082028501610309856103e8565b60005b8481101561034057838303885261032483835161034c565b925061032f826103e8565b60209890980197915060010161030c565b50909695505050505050565b6000610357826103ee565b80845261036b8160208601602086016103fe565b6103748161042e565b9093016020019392505050565b6020808252810161039281846102e5565b9392505050565b60405181810167ffffffffffffffff811182821017156103b857600080fd5b604052919050565b600067ffffffffffffffff8211156103d757600080fd5b506020601f91909101601f19160190565b60200190565b5190565b82818337506000910152565b60005b83811015610419578181015183820152602001610401565b83811115610428576000848401525b50505050565b601f01601f1916905600a265627a7a72305820472f4af95520daa1e5330ed1beabc53a05f2eff7d536a3693ce16fbaec96385b6c6578706572696d656e74616cf50037"
+var ValidatorsBin = "0x60e06040527364d4edefe8ba86d3588b213b0a053e7b910cad686080908152736a39b688d591ea00c9ea69658438794204b5cc6260a052734a14d788d86d021670ebcece1196631d6659598460c05261005c90600090600361006f565b5034801561006957600080fd5b506100fb565b8280548282559060005260206000209081019282156100c4579160200282015b828111156100c45782518254600160a060020a031916600160a060020a0390911617825560209092019160019091019061008f565b506100d09291506100d4565b5090565b6100f891905b808211156100d0578054600160a060020a03191681556001016100da565b90565b6102828061010a6000396000f30060806040526004361061004b5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416633d62062881146100505780634d238c8e1461007b575b600080fd5b34801561005c57600080fd5b5061006561009d565b6040516100729190610214565b60405180910390f35b34801561008757600080fd5b5061009b610096366004610188565b61010c565b005b6060600080548060200260200160405190810160405280929190818152602001828054801561010257602002820191906000526020600020905b815473ffffffffffffffffffffffffffffffffffffffff1681526001909101906020018083116100d7575b5050505050905090565b600080546001810182559080527f290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e56301805473ffffffffffffffffffffffffffffffffffffffff191673ffffffffffffffffffffffffffffffffffffffff92909216919091179055565b6000610181823561022f565b9392505050565b60006020828403121561019a57600080fd5b60006101a68484610175565b949350505050565b6101b78161022f565b82525050565b60006101c88261022b565b8084526020840193506101da83610225565b60005b8281101561020a576101f08683516101ae565b6101f982610225565b6020969096019591506001016101dd565b5093949350505050565b6020808252810161018181846101bd565b60200190565b5190565b73ffffffffffffffffffffffffffffffffffffffff16905600a265627a7a72305820aa395d36a868c31f84352d0f9d02c290a15c02b40f925d0f431d8290c08617016c6578706572696d656e74616cf50037"
 
 // DeployValidators deploys a new Ethereum contract, binding an instance of Validators to it.
 func DeployValidators(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Validators, error) {
@@ -195,52 +195,49 @@ func (_Validators *ValidatorsTransactorRaw) Transact(opts *bind.TransactOpts, me
 	return _Validators.Contract.contract.Transact(opts, method, params...)
 }
 
-// AddValidator is a free data retrieval call binding the contract method 0xb5da04f5.
+// GetValidatorsList is a free data retrieval call binding the contract method 0x3d620628.
 //
-// Solidity: function addValidator(string validatorPubkey) view returns()
-func (_Validators *ValidatorsCaller) AddValidator(opts *bind.CallOpts, validatorPubkey string) error {
-	var ()
-	out := &[]interface{}{}
-	err := _Validators.contract.Call(opts, out, "addValidator", validatorPubkey)
-	return err
-}
-
-// AddValidator is a free data retrieval call binding the contract method 0xb5da04f5.
-//
-// Solidity: function addValidator(string validatorPubkey) view returns()
-func (_Validators *ValidatorsSession) AddValidator(validatorPubkey string) error {
-	return _Validators.Contract.AddValidator(&_Validators.CallOpts, validatorPubkey)
-}
-
-// AddValidator is a free data retrieval call binding the contract method 0xb5da04f5.
-//
-// Solidity: function addValidator(string validatorPubkey) view returns()
-func (_Validators *ValidatorsCallerSession) AddValidator(validatorPubkey string) error {
-	return _Validators.Contract.AddValidator(&_Validators.CallOpts, validatorPubkey)
-}
-
-// GetValidators is a free data retrieval call binding the contract method 0xb7ab4db5.
-//
-// Solidity: function getValidators() view returns(string[])
-func (_Validators *ValidatorsCaller) GetValidators(opts *bind.CallOpts) ([]string, error) {
+// Solidity: function getValidatorsList() view returns(address[])
+func (_Validators *ValidatorsCaller) GetValidatorsList(opts *bind.CallOpts) ([]common.Address, error) {
 	var (
-		ret0 = new([]string)
+		ret0 = new([]common.Address)
 	)
 	out := ret0
-	err := _Validators.contract.Call(opts, out, "getValidators")
+	err := _Validators.contract.Call(opts, out, "getValidatorsList")
 	return *ret0, err
 }
 
-// GetValidators is a free data retrieval call binding the contract method 0xb7ab4db5.
+// GetValidatorsList is a free data retrieval call binding the contract method 0x3d620628.
 //
-// Solidity: function getValidators() view returns(string[])
-func (_Validators *ValidatorsSession) GetValidators() ([]string, error) {
-	return _Validators.Contract.GetValidators(&_Validators.CallOpts)
+// Solidity: function getValidatorsList() view returns(address[])
+func (_Validators *ValidatorsSession) GetValidatorsList() ([]common.Address, error) {
+	return _Validators.Contract.GetValidatorsList(&_Validators.CallOpts)
 }
 
-// GetValidators is a free data retrieval call binding the contract method 0xb7ab4db5.
+// GetValidatorsList is a free data retrieval call binding the contract method 0x3d620628.
 //
-// Solidity: function getValidators() view returns(string[])
-func (_Validators *ValidatorsCallerSession) GetValidators() ([]string, error) {
-	return _Validators.Contract.GetValidators(&_Validators.CallOpts)
+// Solidity: function getValidatorsList() view returns(address[])
+func (_Validators *ValidatorsCallerSession) GetValidatorsList() ([]common.Address, error) {
+	return _Validators.Contract.GetValidatorsList(&_Validators.CallOpts)
+}
+
+// AddValidator is a paid mutator transaction binding the contract method 0x4d238c8e.
+//
+// Solidity: function addValidator(address validatorAddress) returns()
+func (_Validators *ValidatorsTransactor) AddValidator(opts *bind.TransactOpts, validatorAddress common.Address) (*types.Transaction, error) {
+	return _Validators.contract.Transact(opts, "addValidator", validatorAddress)
+}
+
+// AddValidator is a paid mutator transaction binding the contract method 0x4d238c8e.
+//
+// Solidity: function addValidator(address validatorAddress) returns()
+func (_Validators *ValidatorsSession) AddValidator(validatorAddress common.Address) (*types.Transaction, error) {
+	return _Validators.Contract.AddValidator(&_Validators.TransactOpts, validatorAddress)
+}
+
+// AddValidator is a paid mutator transaction binding the contract method 0x4d238c8e.
+//
+// Solidity: function addValidator(address validatorAddress) returns()
+func (_Validators *ValidatorsTransactorSession) AddValidator(validatorAddress common.Address) (*types.Transaction, error) {
+	return _Validators.Contract.AddValidator(&_Validators.TransactOpts, validatorAddress)
 }
