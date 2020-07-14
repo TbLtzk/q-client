a2ddc2797a217cfe4a4692c07e8ade45e95f8a6e
rene <41963722+renaynay@users.noreply.github.com>
2020-07-14 10:35:32 +0200

    cmd/clef: change --rpcport to --http.port and update flags in docs (#21318)

--- cmd/evm/main.go
+++ cmd/evm/main.go
@@ -24,6 +24,7 @@ import (
 
 	"gitlab.com/q-dev/q-client/cmd/evm/internal/t8ntool"
 	"gitlab.com/q-dev/q-client/cmd/utils"
+	"gitlab.com/q-dev/q-client/internal/flags"
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -31,7 +32,7 @@ var gitCommit = "" // Git SHA1 commit hash of the release (set via linker flags)
 var gitDate = ""
 
 var (
-	app = utils.NewApp(gitCommit, gitDate, "the evm command line interface")
+	app = flags.NewApp(gitCommit, gitDate, "the evm command line interface")
 
 	DebugFlag = cli.BoolFlag{
 		Name:  "debug",
@@ -180,7 +181,7 @@ func init() {
 		stateTestCommand,
 		stateTransitionCommand,
 	}
-	cli.CommandHelpTemplate = utils.OriginCommandHelpTemplate
+	cli.CommandHelpTemplate = flags.OriginCommandHelpTemplate
 }
 
 func main() {
