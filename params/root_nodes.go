e481b57149f4157ac32ff95ad0bea58ee994ff6a
v.bochko <v.y.bochko@gmail.com>
2020-08-20 15:54:45 +0300

    QDEV-133 wip: store initial root node list in source code.

---
+++ params/root_nodes.go
@@ -0,0 +1,15 @@
+package params
+
+import (
+	"gitlab.com/q-dev/go-ethereum/common"
+	"gitlab.com/q-dev/go-ethereum/governance"
+)
+
+// DevnetRootNodes - initial root nodes list on devnet.
+var DevnetRootNodes = governance.RootList{
+	Timestamp: 1597927813,
+	Nodes: []common.Address{
+		common.HexToAddress("0x42b9e4ffa28bc4bd9c8f7207f3a296ce960a365d"),
+		common.HexToAddress("0x7c7df968bb4aff69b6f613c713c404f88de394d3"),
+	},
+}
