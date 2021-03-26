88ad076a0c11ade0de7740a3a9d3bf867a4ae410
Péter Szilágyi <peterke@gmail.com>
2021-03-26 14:00:06 +0200

    eth/protocols, metrics, p2p: add handler performance metrics

--- eth/protocols/eth/handler.go
+++ eth/protocols/eth/handler.go
@@ -24,6 +24,7 @@ import (
 	"gitlab.com/q-dev/q-client/common"
 	"gitlab.com/q-dev/q-client/core"
 	"gitlab.com/q-dev/q-client/core/types"
+	"gitlab.com/q-dev/q-client/metrics"
 	"gitlab.com/q-dev/q-client/p2p"
 	"gitlab.com/q-dev/q-client/p2p/enode"
 	"gitlab.com/q-dev/q-client/p2p/enr"
@@ -241,7 +242,14 @@ func handleMessage(backend Backend, peer *Peer) error {
 	} else if peer.Version() >= ETH66 {
 		handlers = eth66
 	}
-
+	// Track the emount of time it takes to serve the request and run the handler
+	if metrics.Enabled {
+		h := fmt.Sprintf("%s/%s/%d/%#02x", p2p.HandleHistName, ProtocolName, peer.Version(), msg.Code)
+		defer func(start time.Time) {
+			sampler := func() metrics.Sample { return metrics.NewExpDecaySample(1028, 0.015) }
+			metrics.GetOrRegisterHistogramLazy(h, nil, sampler).Update(time.Since(start).Microseconds())
+		}(time.Now())
+	}
 	if handler := handlers[msg.Code]; handler != nil {
 		return handler(backend, msg, peer)
 	}
