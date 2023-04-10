59523ae2a398ec2b7535c413df762cd441cf62e8
Serhii Datsenko <sergdacenko@gmail.com>
2023-04-10 13:03:00 +0000

    refactor: updated changelog, version tag and dns prefix

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	QVersionMajor = 1        // Q-Client major version component of the current release
 	QVersionMinor = 3        // Q-Client minor version component of the current release
 	QVersionPatch = 0        // Q-Client patch version component of the current release
-	QVersionMeta  = "alpha2" // Q-Client version metadata to append to the version string
+	QVersionMeta  = "stable" // Q-Client version metadata to append to the version string
 )
 
 const (
