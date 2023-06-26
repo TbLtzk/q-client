6f880021e02308cf357b9ce48144e44c88ff30c4
alexey.naumchuk <alexey.naumchuk@distributedlab.com>
2023-06-26 10:57:55 +0200

    Add methods for receiving layer zero lists from gov

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -28,6 +28,7 @@ import (
 	"gitlab.com/q-dev/q-client/common"
 	"gitlab.com/q-dev/q-client/common/hexutil"
 	"gitlab.com/q-dev/q-client/core/types"
+	"gitlab.com/q-dev/q-client/governance"
 	"gitlab.com/q-dev/q-client/indexer"
 	"gitlab.com/q-dev/q-client/rpc"
 )
@@ -663,6 +664,70 @@ func (ec *Client) SendTransaction(ctx context.Context, tx *types.Transaction) er
 		return err
 	}
 	return ec.c.CallContext(ctx, nil, "eth_sendRawTransaction", hexutil.Encode(data))
+}
+
+func (ec *Client) GetActiveExclusionList(ctx context.Context) (governance.ExclusionList, error) {
+	method := "gov_activeExclusionList"
+
+	var res governance.ExclusionList
+	err := ec.c.CallContext(ctx, &res, method)
+
+	if herr, ok := err.(rpc.Error); ok {
+		if herr.ErrorCode() == -32601 {
+			method = "govPub_activeExclusionList"
+			err = ec.c.CallContext(ctx, &res, method)
+		}
+	}
+
+	return res, err
+}
+
+func (ec *Client) GetProposedExclusionList(ctx context.Context) (governance.ExclusionList, error) {
+	method := "gov_proposedExclusionList"
+
+	var res governance.ExclusionList
+	err := ec.c.CallContext(ctx, &res, method)
+
+	if herr, ok := err.(rpc.Error); ok {
+		if herr.ErrorCode() == -32601 {
+			method = "govPub_proposedExclusionList"
+			err = ec.c.CallContext(ctx, &res, method)
+		}
+	}
+
+	return res, err
+}
+
+func (ec *Client) GetActiveRootList(ctx context.Context) (governance.RootList, error) {
+	method := "gov_activeRootList"
+
+	var res governance.RootList
+	err := ec.c.CallContext(ctx, &res, method)
+
+	if herr, ok := err.(rpc.Error); ok {
+		if herr.ErrorCode() == -32601 {
+			method = "govPub_activeRootList"
+			err = ec.c.CallContext(ctx, &res, method)
+		}
+	}
+
+	return res, err
+}
+
+func (ec *Client) GetProposedRootList(ctx context.Context) (governance.RootList, error) {
+	method := "gov_proposedRootList"
+
+	var res governance.RootList
+	err := ec.c.CallContext(ctx, &res, method)
+
+	if herr, ok := err.(rpc.Error); ok {
+		if herr.ErrorCode() == -32601 {
+			method = "govPub_proposedRootList"
+			err = ec.c.CallContext(ctx, &res, method)
+		}
+	}
+
+	return res, err
 }
 
 func toBlockNumArg(number *big.Int) string {
