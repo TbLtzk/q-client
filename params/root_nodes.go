f0d111c84e3ea134e028f07681f51cc6966e0032
Tobias Latzke <tl.qcore.team@gmail.com>
2026-06-08 15:20:52 +0200

    v2.3.0-rc4

--- params/root_nodes.go
+++ params/root_nodes.go
@@ -15,7 +15,7 @@ var DevnetRootNodes = common.RootList{
 
 // TestnetRootNodes - initial root nodes list on testnet.
 var TestnetRootNodes = common.RootList{
-	Timestamp: 0x607843b9,
+	Timestamp: 0x6a26c125,
 	Nodes: []common.Address{
 		common.HexToAddress("0x64D4edeFE8bA86d3588B213b0A053e7B910Cad68"), // Miner 1
 		common.HexToAddress("0x4a14D788D86D021670EBcecE1196631d66595984"), // Miner 3
